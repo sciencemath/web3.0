aa2ae6c635737e45f428be440ebbb26bb7e13765
Mathais <matrixmachinelearning@gmail.com>
2021-12-27 13:33:35 -0800

    testing gas of eth

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-ethers";
+import "hardhat-gas-reporter";
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
---
+++ test/TestGas.ts
@@ -0,0 +1,18 @@
+import "@nomiclabs/hardhat-ethers";
+import { ethers } from "hardhat";
+
+describe("TestGas", () => {
+  it("Test", async () => {
+    const Gas = await ethers.getContractFactory("TestGas");
+    const gas = await Gas.deploy();
+    await gas.deployed();
+
+    for (let i = 0; i < 10; i++) {
+      await gas.test1();
+      await gas.test2();
+      await gas.test3();
+      await gas.test4();
+      await gas.test5();
+    }
+  });
+});
